51e8110351b71ca2fede9a3bff7f3c9d34cfeafe
Paula <paulaoorjuela@gmail.com>
2025-02-28 15:47:52 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main class="parent">
+        <div class="div1">1</div>
+        <div class="div2">2</div>
+        <div class="div3">3</div>
+        <div class="div4">4</div>
+        <div class="div5">5</div>
+        <div class="div6">6</div>
+        <div class="div7">7</div>
+        <div class="div9">9</div>
+        <div class="div10">10</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
