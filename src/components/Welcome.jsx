059a33a86707d1f64d6dc60de6b8264e66e4a595
Paula <paulaoorjuela@gmail.com>
2025-03-04 19:22:22 -0500

    add fonts and tailwind

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,13 @@
 export default function Welcome() {
     return (
-    <div className="div1 white-background">Welcome</div>
+    <article className="welcome-card white-background raleway-regular flex justify-between">
+        <div className="text-[40px] flex flex-col justify-center items-start gap-2.5">
+            <p><span className="raleway-semiBold">Hi there! <br /> </span>I’m Paula</p>
+            <p>Welcome to <br />my <span className="raleway-semiBold">portfolio!</span></p>
+        </div>
+        <div>
+            image
+        </div>
+    </article>
     )
 }
